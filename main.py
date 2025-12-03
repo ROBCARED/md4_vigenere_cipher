4632177e5a3b44135192c5587474900c4ad9cb18
ROBCARED <robertfollivi49@gmail.com>
2025-12-03 04:07:29 +0100

    feature: cesar_uncipher

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ def cesar_cipher(message, key):
 		crypted_message += crypted_char
 
 	return crypted_message
+
+def cesar_uncipher(crypted_message, key):
+	return cesar_cipher(crypted_message, -key)
+
 crypted_message = cesar_cipher(message="lapin", key=3)
 print(crypted_message)
+
+initial_message = cesar_uncipher(crypted_message=crypted_message, key=3)
+print(initial_message)
